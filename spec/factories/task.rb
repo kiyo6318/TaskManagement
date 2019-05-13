c43c76a75e7cd9e1181175cd4a4f38432a45e0bd
前川清成 <kiyonari6318@MacbookPro.local>
2019-05-13 17:11:48 +0900

    Add: 終了期限でソート出来るのかのテスト追加

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -2,19 +2,22 @@ FactoryBot.define do
 
   factory :task do
     title {"タイトル1"}
-    content {"本文1 "}
+    content {"本文1"}
+    deadline {"2020-03-01"}
     created_at {Time.current + 1.days}
   end
 
   factory :second_task,class: Task do
     title {"タイトル2"}
     content {"本文2"}
+    deadline {"2020-02-01"}
     created_at {Time.current + 2.days}
   end
 
   factory :third_task,class: Task do
     title {"タイトル3"}
     content {"本文3"}
+    deadline {"2020-01-01"}
     created_at {Time.current + 3.days}
   end
 end
