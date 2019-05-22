eeae7bf3f07dd5ae3ab63e64b1614a2394d3f001
前川清成 <kiyonari6318@MacbookPro.local>
2019-05-22 17:39:53 +0900

    Add: ユーザーの管理機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   has_secure_password
   validates :name,presence:true,length:{maximum:30}
   validates :email,presence:true,length:{maximum:255},uniqueness: true
-  has_many :tasks
+  has_many :tasks,dependent: :destroy
 end
